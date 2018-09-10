0ffbaa3e34def1354f0ef81c443c512d40143df9
Frederick Eaton <frederik-bcc@ofb.net>
2018-09-10 15:16:00 -0700

    Add make-glossary. Changes to table-to-cards, ReadTable.pm. Search tools

--- perl/ReadTable.pm
+++ perl/ReadTable.pm
@@ -2,7 +2,7 @@ package ReadTable;
 
 require Exporter;
 @ISA=qw(Exporter);
-@EXPORT=qw(read_table format_group *foot_fmt *tab_fn *curr_line);
+@EXPORT=qw(read_vocab_table format_group *foot_fmt *tab_fn *curr_line);
 
 use warnings;
 use strict;
@@ -27,13 +27,13 @@ sub format_group {
   return $str;
 }
 
-sub read_table ($$) {
+sub read_vocab_table ($$) {
   my ($callback, $tab_files) = @_;
   for my $tf (@$tab_files) {
     $tab_fn = $tf;
     -e $tab_fn or die "Table file $tab_fn does not exist\n";
 
-    warn "Reading from $tab_fn" if $verbose;
+    warn "Reading from $tab_fn\n" if $verbose;
 
     open IN, "<", $tab_fn or
         die "Couldn't open $tab_fn for reading\n";
@@ -51,7 +51,16 @@ sub read_table ($$) {
       # remove spaces
       @c = map { s/^\s*(.*?)\s*$/$1/; $_ } @c;
 
-      &$callback(@c);
+      # at this point we make assumptions about table columns, this
+      # code could be moved to another routine
+      my ($arabic,$english,$group,$notes) = @c;
+      my ($foot) = format_group $group;
+      my ($artransl);
+      if($arabic =~ /^(.*?)\s*\|\|\s*(.*)$/) {
+        ($arabic, $artransl) = ($1, $2);
+      }
+      
+      &$callback($arabic,$artransl,$english,$foot,$notes);
     }
     close IN;
   }
