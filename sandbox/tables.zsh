8117b2736feeac086fbbdce69170308808f9051a
Frederick Eaton <frederik-bcc@ofb.net>
2018-09-12 14:55:44 -0700

    TABLES from Makefile

---
+++ sandbox/tables.zsh
@@ -0,0 +1,8 @@
+TABLES=(letters-positions.tab numerals.tab \
+	bcc-workbook-words.tab ma-unit-1-4.tab ma-unit-5-7.tab \
+	ma-unit-9-10.tab ma-unit-11-13.tab \
+	ma-unit-14.tab ma-unit-16.tab \
+	classroom-vocab.tab \
+	colors.tab pronouns-possessives.tab \
+	capitals.tab \
+	verb-conj-ktb.tab)
