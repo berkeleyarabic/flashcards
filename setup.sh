8ef94aafc9e2487364e2a58e6fcc9aa8f0a1a99d
bccfrederick <frederik-bcc@ofb.net>
2019-01-29 00:34:25 -0800

    Script to set environment variables for this project

---
+++ setup.sh
@@ -0,0 +1,8 @@
+
+# set some environment variables to help use this software
+export PERL5LIB=$PWD/perl
+export PATH=$PWD:$PATH
+
+# make perl assume UTF8 arguments
+# http://blog.thewebsitepeople.org/2012/06/perl-default-to-utf-8-encoding/
+export PERL5OPT=-CA
