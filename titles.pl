6398a0ebda939bb59186fa0422c3f5ef00a2b759
bccfrederick <frederik-bcc@ofb.net>
2019-01-30 09:57:28 -0800

    Do titles for all existing decks

--- titles.pl
+++ titles.pl
@@ -1,1 +1,13 @@
-$doctitle =~ s/^ma-unit-(\d+)$/Mastering Arabic 1 Unit $1/;
+if($doctitle =~ /^ma-unit-([\d-]+)$/) {
+  $doctitle = "Mastering Arabic 1 Unit $1";
+} elsif($doctitle eq "verb-conj-ktb") {
+  $doctitle = "Verb Conjugations (Kataba)"
+} elsif($doctitle eq "pronouns-possessives") {
+  $doctitle = "Pronouns and Possessives"
+} else {
+  $doctitle =~ s/^bas/BAS/;
+  $doctitle =~ s/^bcc/BCC/;
+  $doctitle =~ s/^uc/UC/;
+  $doctitle =~ s/-/ /g;
+  $doctitle =~ s/(^| )(.)/$1.(ucfirst $2)/eg;
+}
